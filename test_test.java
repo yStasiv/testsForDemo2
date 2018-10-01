b7b695883964534ab58d3d4206468a5191771ded
Yurii Stasiv <Yurastasiv@hotmail.com>
2018-10-01 11:06:35 +0300

    corect code and arr text of presentation

--- test_test.java
+++ test_test.java
@@ -35,48 +35,66 @@ public class test_test {
 		driver.quit();
 	}
 
-	
+	//@Test
+	public void ChangeNumOfItemsInCart() throws Exception {
+			//Add MacBook to cart
+					driver.findElement(By.linkText("MacBook")).click();
+					driver.findElement(By.cssSelector("#button-cart")).click();
+					System.out.println("McBook was added to cart...");
+					// Thread.sleep(2000); // For demonstration
+			// Check if user see correct message about adding good to cart
+					Assert.assertTrue(driver.findElement(By.cssSelector(".alert-success"))
+							.getText().contains("Success: You have added MacBook to your shopping cart!"));
+					System.out.println("User see correct message, good was added...");
+					// Thread.sleep(2000); // For demonstration
+			//Check if cart button contain correct data
+					Assert.assertEquals(driver.findElement(By.cssSelector("#cart")).getText(), "1 item(s) - $602.00");
+			//Open cart
+					driver.manage().window().maximize();
+					driver.findElement(By.linkText("Shopping Cart")).click();
+					System.out.println("Shopping Cart page was opened...");
+					// Thread.sleep(2000); // For demonstration
+			//Edit quantity
+					driver.findElement(By.xpath("//*[@id=\"content\"]/form/div/table/tbody/tr/td[4]/div/input")).clear();
+					driver.findElement(By.xpath("//*[@id=\"content\"]/form/div/table/tbody/tr/td[4]/div/input")).sendKeys("5");
+					driver.findElement(By.cssSelector(".fa-refresh")).click();
+			//Check if user see correct message about modified cart
+					Assert.assertTrue(driver.findElement(By.cssSelector(".alert-success"))
+							.getText().contains("Success: You have modified your shopping cart!"));
+					System.out.println("User see correct message, cart was modified...");
+			//Check if quantity in cart display correctly
+					driver.findElement(By.cssSelector("#cart")).click();
+					Assert.assertEquals(driver
+							.findElement(By.cssSelector(
+									"#cart > ul > li:nth-child(2) > div > table > tbody > tr:nth-child(2) > td:nth-child(2)")).getText(), "$10.00");// eco tax
+					System.out.println("Quantity in cart display correct...");
+					System.out.println("Positiv quantity editing worc correctly");
+	}
 
 	@Test
-	public void AddSingleItemToCart() throws Exception {
-		//Check if cart is empty
-				Assert.assertEquals(driver.findElement(By.cssSelector("#cart-total")).getText(), "0 item(s) - $0.00");
-		//Add MacBook to cart
-				driver.findElement(By.linkText("MacBook")).click();
-				driver.findElement(By.cssSelector("#button-cart")).click();
-				System.out.println("McBook was added to cart...");
-				// Thread.sleep(2000); // For demonstration
-		// Check if user see correct message about adding good to cart
-				Assert.assertTrue(
-						driver.findElement(By.cssSelector("body > div:nth-child(4) > div.alert.alert-success")).getText()
-								.contains("Success: You have added MacBook to your shopping cart!"));
-				System.out.println("User see correct message, good was added...");
-		//Check if cart button contain correct data
-				Assert.assertEquals(driver.findElement(By.cssSelector("#cart")).getText(), "1 item(s) - $602.00");
-		//back to main page		
-				driver.get("http://atqc-shop.epizy.com");
-		//Add iPhone to cart		
-				driver.findElement(By.linkText("iPhone")).click();
-				driver.findElement(By.cssSelector("#button-cart")).click();
-				System.out.println("iPhone was added to cart...");
-				// Thread.sleep(2000); // For demonstration
-		// Check if user see correct message about adding good to cart
-				Assert.assertTrue(
-						driver.findElement(By.cssSelector("body > div:nth-child(4) > div.alert.alert-success")).getText()
-								.contains("Success: You have added iPhone to your shopping cart!"));
-				System.out.println("User see correct message, good was added...");
-		//Check if cart button contain correct data
-				Assert.assertEquals(driver.findElement(By.cssSelector("#cart")).getText(), "2 item(s) - $725.20");
-				driver.findElement(By.cssSelector("#cart")).click();
-		//Check if the tax is counted correctly
-				Assert.assertEquals(driver
-						.findElement(By.cssSelector(
-								"#cart > ul > li:nth-child(2) > div > table > tbody > tr:nth-child(2) > td:nth-child(2)")).getText(), "$2.00");// eco tax
-				System.out.println("Eco Tax display correct...");			
-//				
-				
-				
-				
-				
+				public void ErrorMessageChangeNumOfItemsInCart() throws Exception {
+					//Add MacBook to cart
+					driver.findElement(By.linkText("MacBook")).click();
+					driver.findElement(By.cssSelector("#button-cart")).click();
+					System.out.println("McBook was added to cart...");
+					// Thread.sleep(2000); // For demonstration
+			// Check if user see correct message about adding good to cart
+					Assert.assertTrue(driver.findElement(By.cssSelector(".alert-success"))
+							.getText().contains("Success: You have added MacBook to your shopping cart!"));
+					System.out.println("User see correct message, good was added...");
+					// Thread.sleep(2000); // For demonstration
+			//Check if cart button contain correct data
+					Assert.assertEquals(driver.findElement(By.cssSelector("#cart")).getText(), "1 item(s) - $602.00");
+			//Open cart
+					driver.manage().window().maximize();
+					driver.findElement(By.linkText("Shopping Cart")).click();
+					System.out.println("Shopping Cart page was opened...");
+					// Thread.sleep(2000); // For demonstration
+			//Edit quantity
+					driver.findElement(By.xpath("//*[@id=\"content\"]/form/div/table/tbody/tr/td[4]/div/input")).clear();
+					driver.findElement(By.xpath("//*[@id=\"content\"]/form/div/table/tbody/tr/td[4]/div/input")).sendKeys("test");
+			//Check Error message
+					Assert.assertTrue(driver.findElement(By.cssSelector(".alert-success"))
+							.getText().contains("Warning: Input valid data!"));
 	}
 }
